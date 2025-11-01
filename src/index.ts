6883f2a0f63771cc4939e393ddfee4e44646f2da
JunNagashima <j.nagashima.carecon@gmail.com>
2025-11-01 15:19:08 +0900

    [feature] cors 追加

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,19 @@
 import express, { Request, Response } from 'express';
 import { PrismaClient } from '@prisma/client';
+import cors from "cors";
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 const prisma = new PrismaClient();
+const allowedOrigins = [process.env.FRONTEND_URL!];
+
 
 app.use(express.json());
+app.use(cors({ origin: allowedOrigins, credentials: true }));
 
 app.get('/', async (req: Request, res: Response) => {
-    const sampleData = await prisma.sample.findMany();
-    res.json({ message: 'Hello, World!', data: sampleData });
+    const sampleData = await prisma.sample.findFirst();
+    res.json(sampleData);
 });
 
 app.listen(PORT, () => {
